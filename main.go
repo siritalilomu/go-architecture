c1c475bb622e3396285365ac35de3d33ecb4b101
siritali <siritali@STL.local>
2020-10-09 10:04:16 -0600

    unmarshal

--- main.go
+++ main.go
@@ -22,5 +22,12 @@ func main() {
 	if err != nil {
 		log.Panic(err)
 	}
-	fmt.Println(string(bs))
+	fmt.Println("print json:", string(bs))
+
+	xp2 := []person{}
+	err = json.Unmarshal(bs, &xp2)
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Println("back into a Go data structure:", xp2)
 }
