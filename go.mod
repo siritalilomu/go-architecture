d2f0b80b53ddce5786c12c670a31d015deb07204
siritali <siritali@STL.local>
2020-10-09 12:18:17 -0600

    hash password w/ bcrypt

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/siritalilomu/go-architecture
 
 go 1.13
+
+require golang.org/x/crypto v0.0.0-20201002170205-7f63de1d35b0
--- main.go
+++ main.go
@@ -1,11 +1,37 @@
 package main
 
 import (
-	"encoding/base64"
 	"fmt"
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
-	fmt.Println(base64.StdEncoding.EncodeToString([]byte("username:password"))) // dXNlcm5hbWU6cGFzc3dvcmQ=
-	// curl -u username:password -v google.com
+	password := "123456789"
+
+	hp, err := hashPassword(password)
+	if err != nil {
+		log.Fatalln("Not logged in.")
+	}
+
+	if err := comparePassword(password, hp); err != nil {
+		log.Fatalln("Not logged in.")
+	}
+	log.Println("Logged in!")
+}
+
+func hashPassword(password string) ([]byte, error) {
+	bs, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, fmt.Errorf("error while generating bcrypt hash from password: %w", err)
+	}
+	return bs, nil
+}
+
+func comparePassword(password string, hashedPassword []byte) error {
+	if err := bcrypt.CompareHashAndPassword(hashedPassword, []byte(password)); err != nil {
+		return fmt.Errorf("Invalid Username and Password: %w", err)
+	}
+	return nil
 }
