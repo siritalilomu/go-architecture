f3429793fc1f2db0621ef64b61271ddd55f2883b
siritali <siritali@STL.local>
2020-10-09 13:02:27 -0600

    sign and check signature

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/hmac"
+	"crypto/sha512"
 	"fmt"
 	"log"
 
@@ -34,4 +36,27 @@ func comparePassword(password string, hashedPassword []byte) error {
 		return fmt.Errorf("Invalid Username and Password: %w", err)
 	}
 	return nil
+}
+
+func signMessage(msg []byte) ([]byte, error) {
+	// var key = []byte{}
+	// for i := 1; i <= 64; i++ {
+	// 	key = append(key, byte(i))
+	// }
+	key := []byte("mysecretkey")
+	h := hmac.New(sha512.New, key)
+	if _, err := h.Write(msg); err != nil {
+		return nil, fmt.Errorf("Error in singMessage while hashing message: %w", err)
+	}
+	signature := h.Sum(nil)
+	return signature, nil
+}
+
+func checkSignature(msg, signature []byte) (bool, error) {
+	newSignature, err := signMessage(msg)
+	if err != nil {
+		return false, fmt.Errorf("Error in checkSignature while getting signature of message: %w", err)
+	}
+	same := hmac.Equal(newSignature, signature)
+	return same, nil
 }
