a67b5fcd7083e58855c36b078f7e82c37fc382d9
siritali <siritali@STL.local>
2020-10-09 11:54:45 -0600

    basic auth header

--- main.go
+++ main.go
@@ -1,42 +1,11 @@
 package main
 
 import (
-	"encoding/json"
+	"encoding/base64"
 	"fmt"
-	"log"
-	"net/http"
 )
 
-type person struct {
-	Name string
-}
-
 func main() {
-	http.HandleFunc("/encode", en)
-	http.HandleFunc("/decode", de)
-	http.ListenAndServe(":8080", nil)
+	fmt.Println(base64.StdEncoding.EncodeToString([]byte("username:password"))) // dXNlcm5hbWU6cGFzc3dvcmQ=
+	// curl -u username:password -v google.com
 }
-
-func en(w http.ResponseWriter, r *http.Request) {
-	p1 := person{
-		Name: "Siri",
-	}
-	p2 := person{
-		Name: "Keaton",
-	}
-	people := []person{p1, p2}
-	if err := json.NewEncoder(w).Encode(people); err != nil {
-		log.Println("Encoded bad data", err)
-	}
-}
-
-func de(w http.ResponseWriter, r *http.Request) {
-	people := []person{}
-	if err := json.NewDecoder(r.Body).Decode(&people); err != nil {
-		log.Println("Decoded bad data", err)
-	}
-	fmt.Println("People: ", people)
-}
-
-// command to test decoding json
-// curl -XGET -H "Content-type: application/json" -d '[{"Name":"Siri Tali"}]' 'localhost:8080/decode'
